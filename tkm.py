c622f7830130c113f95927ae6afad0869da0c8f8
EnesKaratekin09 <117197137+EnesKaratekin09@users.noreply.github.com>
2023-06-19 22:42:16 +0300

    Add files via upload

--- tkm.py
+++ tkm.py
@@ -1,33 +1,25 @@
 import random
 secim_listesi=["Taş","Kağıt","Makas"]
-
+isim=input("Adınız nedir?\n")
+print("Merhaba",isim)
 def oyun():
-    ugur=random.choice(secim_listesi)
-    enes=random.choice(secim_listesi)
-    if ugur=="Taş" and enes=="Taş":
-        kazanan="Berabere."
-    if ugur=="Taş" and enes=="Kağıt":
-        kazanan="Enes kazandı."
-    if ugur=="Taş" and enes=="Makas":
-        kazanan="Uğur kazandı."
-    if ugur=="Kağıt" and enes=="Taş":
-        kazanan="Uğur kazandı."
-    if ugur=="Kağıt" and enes=="Kağıt":
-        kazanan="Berabere."
-    if ugur=="Kağıt" and enes=="Makas":
-        kazanan="Enes kazandı."
-    if ugur=="Makas" and enes=="Taş":
-        kazanan="Enes kazandı."
-    if ugur=="Makas" and enes=="Kağıt":
-        kazanan="Uğur kazandı."
-    if ugur=="Makas" and enes=="Makas":
-        kazanan="Berabere."
-    
-    print("Uğur:",ugur)
-    print("Enes:",enes)
-    print(kazanan)
-    
-    tekrar=input("")
-    if tekrar=="":
-        oyun()
+    while True:
+        secim=input("\n\nTaşı seçmek için T\nKağıtı seçmek için K\nMakası seçmek için M yazınız.\n")
+        pc=random.choice(secim_listesi)
+        if pc=="Taş" and secim=="K":
+            kazanan="Kazandınız."
+        elif pc=="Taş" and secim=="M":
+            kazanan="Bilgisayar kazandı."
+        elif pc=="Kağıt" and secim=="T":
+            kazanan="Bilgisayar kazandı."
+        elif pc=="Kağıt" and secim=="M":
+            kazanan="Kazandınız."
+        elif pc=="Makas" and secim=="K":
+            kazanan="Bilgisayar kazandı."
+        elif pc=="Makas" and secim=="T":
+            kazanan="Kazandınız."
+        else:
+            kazanan="Berabere"
+        print("Bilgisayar ",pc,"yaptı.\n")
+        print(kazanan)
 oyun()
